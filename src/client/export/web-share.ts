abe85f9ffbfc4ba88fe98b7027a0ea6a846863a4
Patrick Brosset <patrickbrosset@gmail.com>
2021-10-04 21:11:15 +0200

    Share as file instead of text

--- src/client/export/web-share.ts
+++ src/client/export/web-share.ts
@@ -1,24 +1,35 @@
 import { showUnsupportedMessage } from '../unsupported';
 import { getText } from '../editor';
 
+// Typescript doesn't know about navigator.canShare yet. Let's teach it.
+declare global {
+    interface Navigator {
+        canShare: (data: {}) => Boolean
+    }
+}
+
 const shareButton = document.querySelector('#web-share');
 
 if (!navigator.share) {
     showUnsupportedMessage(shareButton?.parentElement, 'The Web Share API is not supported');
 }
 
-async function shareFile(text: string) {
+async function shareAsFile(text: string) {
     if (!navigator.share) {
         return;
     }
 
-    try {
-        await navigator.share({ text });
-    } catch (e) {
-        console.error('Error sharing the content', e);
+    const file = new File(text.split('\n'), 'shared-file.txt', { type: 'text/plain' });
+
+    if (navigator.canShare && navigator.canShare({ files: [file] })) {
+        try {
+            await navigator.share({ files: [file] });
+        } catch (e) {
+            console.error('Error sharing the content', e);
+        }
     }
 }
 
 shareButton?.addEventListener('click', async () => {
-    shareFile(getText());
+    shareAsFile(getText());
 });
