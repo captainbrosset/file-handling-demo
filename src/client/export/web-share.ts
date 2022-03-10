0be00054742c18c30353ad9f93ef79eb5aaea6ec
Patrick Brosset <patrickbrosset@gmail.com>
2022-03-10 10:59:25 +0100

    Hide the web share button on unsupporting browsers
    If the navigator.share or navigator.canShare functions
    don't exist, then the button should be hidden.
    But also when canShare({files:[file]}) returns false for a
    simple text file.

--- src/client/export/web-share.ts
+++ src/client/export/web-share.ts
@@ -10,26 +10,47 @@ declare global {
 
 const shareButton = document.querySelector('#web-share');
 
+// Test for support.
+let isSupported = true;
+
 if (!navigator.share) {
     showUnsupportedMessage(shareButton?.parentElement, 'The Web Share API is not supported');
+    shareButton?.remove();
+    isSupported = false;
 }
 
-async function shareAsFile(text: string) {
-    if (!navigator.share) {
-        return;
+if (!navigator.canShare) {
+    showUnsupportedMessage(shareButton?.parentElement, 'The navigator.canShare function is missing');
+    shareButton?.remove();
+    isSupported = false;
+} else {
+    // Create a dummy file to test if the browser can share it with canShare.
+    const testFile = new File(['test'], 'test-file.txt', { type: 'text/plain' });
+    if (!navigator.canShare({ files: [testFile] })) {
+        showUnsupportedMessage(shareButton?.parentElement, 'The browser does not support sharing files');
+        shareButton?.remove();
+        isSupported = false;
     }
+}
 
-    const file = new File(text.split('\n'), 'shared-file.txt', { type: 'text/plain' });
+if (isSupported) {
+    async function shareAsFile(text: string) {
+        if (!navigator.share) {
+            return;
+        }
 
-    if (navigator.canShare && navigator.canShare({ files: [file] })) {
-        try {
-            await navigator.share({ files: [file] });
-        } catch (e) {
-            console.error('Error sharing the content', e);
+        const file = new File(text.split('\n'), 'shared-file.txt', { type: 'text/plain' });
+
+        if (navigator.canShare && navigator.canShare({ files: [file] })) {
+            try {
+                await navigator.share({ files: [file] });
+            } catch (e) {
+                console.error('Error sharing the content', e);
+            }
         }
     }
-}
 
-shareButton?.addEventListener('click', async () => {
-    shareAsFile(getText());
-});
+    shareButton?.addEventListener('click', async () => {
+        shareAsFile(getText());
+    });
+}
